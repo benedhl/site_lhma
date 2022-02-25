13dedac3b7c45a5a2585bb9eb0cc359f281726ec
Mariana Oliveira <marianaoliver233@gmail.com>
2022-02-25 15:37:38 -0300

    Criação da seção 'Planos e Preços', na aba de comentários e da área de contato

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+/*=============== TESTIMONIAL SWIPER ===============*/
+var swiper = new Swiper(".testimonial-wrapper", {
+    loop: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
